45453ddabb738edda589c29d9003a0824ae305d4
joseph <jsensback@gmail.com>
2024-08-23 12:01:52 -0700

    Add project files.

---
+++ PickRandomCardsMAUI/CardPicker.cs
@@ -0,0 +1,43 @@
+﻿namespace PickRandomCards;
+
+internal class CardPicker
+{
+    public static string[] PickSomeCards(int numberOfCards)
+    {
+        string[] pickedCards = new string[numberOfCards];
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+        }
+        return pickedCards;
+    }
+
+    public static string RandomSuit()
+    {
+        int value = Random.Shared.Next(1, 5);
+
+        string suit = value switch
+        {
+            1 => "Hearts",
+            2 => "Clubs",
+            3 => "Spades",
+            _ => "Diamonds",
+        };
+
+        return suit;
+    }
+
+    public static string RandomValue()
+    {
+        int value = Random.Shared.Next(1,14);
+        string card = value switch
+        {
+            1 => "Ace",
+            11 => "Jack",
+            12 => "Queen",
+            13 => "King",
+            _ => value.ToString(),
+        };
+        return card;
+    }
+}
---
+++ PickRandomCardsMAUI/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PickRandomCards;
+namespace PickRandomCardsMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+            string enteredCardNumber = NumberOfCards.Text;
+            if (!string.IsNullOrEmpty(enteredCardNumber)) {
+                if (int.TryParse(enteredCardNumber, out int cardNumber))
+                {
+                    string[] cards = CardPicker.PickSomeCards(cardNumber);
+                    string newHandData = "";
+                    foreach (string card in cards) {
+                        newHandData += card + Environment.NewLine;
+                    }
+                    CardsPickedLabel.Text = newHandData;
+                    CardsPickedLabel.BackgroundColor = Colors.SlateGray;
+                } else
+                {
+                    HeadingLabel.Text = "Please enter a valid card number";
+                }
+            } 
+        }
+    }
+
+}
